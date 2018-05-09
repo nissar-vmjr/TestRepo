f30d6380f2139de40da57ae431ceea3a5678ec49
Nissar Ahmed <nahmed047@nam.ad.pwcinternal.com>
2018-05-09 17:29:27 +0530

    last

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ web3.eth.defaultAccount = web3.eth.accounts[1];
 
 var bondContract=web3.eth.contract(abiData);
 
-// testing comments. few more comments.  added in remote repo
+// testing comments. few more comments.  added in remote repo .. more in local
 
 var bond= bondContract.at(contractAddress);
 console.log(bond);
